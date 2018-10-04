99e817cfbc08cdcfde9578dfba35c433d672fd9b
laskjk <dchuan001@e.ntu.edu.sg>
2018-10-04 05:37:33 -0700

    Code for repeated experimentation added

---
+++ data_analysis.py
@@ -0,0 +1,8 @@
+from mpl_toolkits import mplot3d
+import numpy as np
+
+from scipy import ndimage as ndi
+from skimage import feature
+from skimage.filters import roberts, sobel, scharr, prewitt
+
+from mpl_toolkits.mplot3d import Axes3D
--- gprSuperRealz.py
+++ gprSuperRealz.py
@@ -19,7 +19,7 @@ WalabotAPI = load_source('WalabotAPI',
     'C:/Program Files/Walabot/WalabotSDK/python/WalabotAPI.py')
 WalabotAPI.Init("C:/Program Files/Walabot/WalabotSDK/bin/WalabotAPI.dll")
 
-def GetSignals(targets, counter, filename):
+def GetSignal(targets, counter, filename):
     jsonRes = {}
     jsonRes['time'] = targets[0]
     jsonRes['amplitude'] = targets[1]
@@ -32,13 +32,15 @@ def GetSignals(targets, counter, filename):
     with open(filename, 'w') as f:
         json.dump(data, f)
 
-def PrintSensorTargets(targets, counter, filename):
+def PrintSensorTargets(targets, counter, filename, rasterImage, power):
     system('cls' if platform == 'win32' else 'clear')
+    jsonRes = {}
+    jsonRes['rasterImage'] = rasterImage
+    jsonRes['power'] = power
     if targets:
         for i,target in enumerate(targets):
             res = ('Target #{}:\ntype: {}\nangleDeg: {}\nx: {}\ny: {}\nz: {}' + '\nwidth: {}\namplitude: {}\n').format(i + 1, target.type, target.angleDeg, target.xPosCm, target.yPosCm, target.zPosCm, target.widthCm, target.amplitude)
 
-            jsonRes = {}
             jsonRes['targetNum'] = i + 1
             jsonRes['type'] = target.type
             jsonRes['angleDeg'] = target.angleDeg
@@ -47,6 +49,8 @@ def PrintSensorTargets(targets, counter, filename):
             jsonRes['zPosCm'] = target.zPosCm
             jsonRes['widthCm'] = target.widthCm
             jsonRes['amplitude'] = target.amplitude
+            jsonRes['rasterImage'] = rasterImage
+            jsonRes['power'] = power
 
             with open(filename) as f:
                 data = json.load(f)
@@ -59,10 +63,11 @@ def PrintSensorTargets(targets, counter, filename):
         with open(filename) as f:
             data = json.load(f)
 
-        data.update({counter: None})
+        data.update({counter: jsonRes})
 
         with open(filename, 'w') as f:
             json.dump(data, f)
+
 def InWallApp(filename):
     rawSignalFile = filename+'.json'
     imageFile = filename+'_simple.json'
@@ -79,21 +84,7 @@ def InWallApp(filename):
     # 2) Configure: Set scan profile and arena
     # Set Profile - to Short-range.
     WalabotAPI.SetProfile(WalabotAPI.PROF_SHORT_RANGE_IMAGING)
-    # Set arena by radial coordinates, with arena resolution
 
-    # Set Spherical Coordinates
-    #WalabotAPI.SetArenaR(30, 200, 5)
-
-    # Set polar and resolution of arena
-    # WalabotAPI.SetArenaTheta(-15, 15, 5);
-
-    # Set azimuth range
-    # WalabotAPI.SetArenaPhi(-60, 60, 5);
-
-
-    #WalabotAPI.SetArenaX(xArenaMin, xArenaMax, xArenaRes)
-    #WalabotAPI.SetArenaY(yArenaMin, yArenaMax, yArenaRes)
-    #WalabotAPI.SetArenaZ(zArenaMin, zArenaMax, zArenaRes)
     # Walabot filtering disable
     WalabotAPI.SetDynamicImageFilter(WalabotAPI.FILTER_TYPE_NONE)
     # 3) Start: Start the system in preparation for scanning.
@@ -103,12 +94,13 @@ def InWallApp(filename):
     appStatus = -1
     while appStatus != 4:
         appStatus, calibrationProcess = WalabotAPI.GetStatus()
+
         print("Starting up ", APP_STATUS[appStatus], "percentage", calibrationProcess )
         for _ in range(10):
             WalabotAPI.Trigger()
-
+    pairs = WalabotAPI.GetAntennaPairs();
     counter = 1
-    dog = input("Begin")
+    stopper = input("Begin")
     while True:
         # 5) Trigger: Scan (sense) according to profile and record signals
         # to be available for processing and retrieval.
@@ -116,10 +108,16 @@ def InWallApp(filename):
         # 6) Get action: retrieve the last completed triggered recording
         print("Scanning ",counter)
         pairs = WalabotAPI.GetAntennaPairs();
+        for pair in pairs:
+            if pair.txAntenna == 10 and pair.rxAntenna == 11:
+                chosenPair = pair
+
         targets = [];
-        targets= WalabotAPI.GetSignal(pairs[0]);
+        print(chosenPair)
+        targets= WalabotAPI.GetSignal(chosenPair);
         GetSignal(targets, counter, rawSignalFile)
         print("Obtained ", counter, "raw signal")
+
 
 
 
@@ -130,10 +128,11 @@ def InWallApp(filename):
         # plt.pause(0.5)
         # plt.gcf().clear()
 
-
         targets = WalabotAPI.GetImagingTargets()
-        PrintSensorTargets(targets,counter,imageFile)
-        print("Obtained ", counter, "target")
+        rasterImage, _, _, _, power = WalabotAPI.GetRawImage()
+
+        PrintSensorTargets(targets,counter,imageFile,rasterImage,power)
+        print("Obtained ", counter, "target image")
 
         #alarm when done
         duration = 500
@@ -156,6 +155,7 @@ def InWallApp(filename):
     print ('Terminate successfully')
 if __name__ == '__main__':
     filename = input("Key in experiment name: ")
+    filename = './results/' + filename
     paths = [filename+'.json', filename+'_simple.json']
     for pth in paths:
         if not path.exists(pth):
