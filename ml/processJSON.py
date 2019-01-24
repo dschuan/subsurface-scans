a6eb9cd9349018872b0086f4e2c2f7fc1a388d16
ThursdayLam <jordanlam009@hotmail.com>
2019-01-24 18:02:08 +0800

    update processjson for new data type

--- ml/processJSON.py
+++ ml/processJSON.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import hilbert
-
+from ast import literal_eval as make_tuple
 
 
 def findMin(amplitude):
@@ -37,11 +37,15 @@ def processGroundTruth(point_pairs,array,radius,value):
 
 
 def processJSON(data):
-	keys = data.values()
+	results = data['scan']
+
+
+
 	y_val = []
 	x_val = []
-	for key in keys:
-		coord = key['coord']
+	for result in results:
+		coord = make_tuple(result['coord'])
+		coord = (int(coord[0]),int(coord[1]))
 		y_val.append(coord[1])
 		x_val.append(coord[0])
 
@@ -53,19 +57,22 @@ def processJSON(data):
 
 	print( " X_LENGTH ",X_LENGTH," Y_LENGTH ",Y_LENGTH," Y_OFFSET ",Y_OFFSET," X_OFFSET ",X_OFFSET)
 
-	keys = data.values()
 	scanArray = np.zeros(shape=(X_LENGTH,Y_LENGTH))
-	for key in keys:
-		amp = key['amplitude']
-		coord = key['coord']
+	for result in results:
+		coord = make_tuple(result['coord'])
+		coord = (int(coord[0]),int(coord[1]))
+
+		body = result['body']
+		amp = body[0]['amplitude']
+
 		scanArray[coord[0]-X_OFFSET][coord[1]-Y_OFFSET] = findMin(amp)
 
-
-	plt.imshow(scanArray)
-	plt.show()
-
-	plt.plot(scanArray[:,int(X_LENGTH/2 - X_OFFSET)])
-	plt.show()
+	#
+	# plt.imshow(scanArray)
+	# plt.show()
+	#
+	# plt.plot(scanArray[:,int(X_LENGTH/2 - X_OFFSET)])
+	# plt.show()
 
 	truthArray = np.zeros_like(scanArray)
 	truthArray[13,:] = np.ones(Y_LENGTH)
@@ -75,23 +82,30 @@ def processJSON(data):
 	return (scanArray,truthArray)
 
 if __name__ == "__main__":
-	grid_size = 20
-	array = np.zeros((grid_size,grid_size))
+	# grid_size = 20
+	# array = np.zeros((grid_size,grid_size))
+	#
+	# line = [((0,10),(10,20))]
+	# value = 1
+	# radius = 1
+	# array = processGroundTruth(line,array,radius,value)
+	#
+	# line = [((0,3),(20,8))]
+	# value = 2
+	# radius = 3
+	# array = processGroundTruth(line,array,radius,value)
+	#
+	# line = [((15,0),(1,20))]
+	# value = 3
+	# radius = 0.5
+	# array = processGroundTruth(line,array,radius,value)
+	#
+	# plt.imshow(array)
+	# plt.show()
 
-	line = [((0,10),(10,20))]
-	value = 1
-	radius = 1
-	array = processGroundTruth(line,array,radius,value)
+	with open('../results/metal_ruler.json') as f:
+		data = json.load(f)
+		scanArray,truthArray = processJSON(data)
 
-	line = [((0,3),(20,8))]
-	value = 2
-	radius = 3
-	array = processGroundTruth(line,array,radius,value)
-
-	line = [((15,0),(1,20))]
-	value = 3
-	radius = 0.5
-	array = processGroundTruth(line,array,radius,value)
-
-	plt.imshow(array)
-	plt.show()
+		plt.imshow(scanArray)
+		plt.show()
